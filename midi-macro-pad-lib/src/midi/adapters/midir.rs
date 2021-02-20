b1bfb94be8dcf2015ad4fd94e14c31d8f5f4704a
Michael Duerinckx <mich@michd.me>
2021-02-20 15:30:07 +0000

    Midir: list ports, pick listen port via string

--- midi-macro-pad-lib/src/midi/adapters/midir.rs
+++ midi-macro-pad-lib/src/midi/adapters/midir.rs
@@ -1,11 +1,11 @@
 use crate::midi::types::MidiMessage;
+use midir::{MidiInput, MidiInputPort};
 use num_derive::FromPrimitive;
-use midir::MidiInput;
+use num_traits::FromPrimitive;
 use std::sync::mpsc::SyncSender;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use num_traits::FromPrimitive;
 
 #[derive(FromPrimitive)]
 enum ChannelMessageType {
@@ -30,36 +30,53 @@ impl Midir {
         }
     }
 
-    pub fn start(&mut self, tx: SyncSender<MidiMessage>) -> Option<thread::JoinHandle<()>> {
+    pub fn list_ports() -> Vec<String> {
+        let midi_in = MidiInput::new("MIDI input").unwrap();
+        let ports = midi_in.ports();
+
+        ports
+            .iter()
+            .map(|p| {
+                midi_in
+                    .port_name(p)
+                    .unwrap_or(String::from("(unknown port)"))
+            })
+            .collect()
+    }
+
+    pub fn start(
+        &mut self,
+        port_pattern: String,
+        tx: SyncSender<MidiMessage>,
+    ) -> Option<thread::JoinHandle<()>> {
         let active = Arc::clone(&self.active);
         let mut is_active = active.lock().unwrap();
-        if *is_active { return None; }
+        if *is_active {
+            return None;
+        }
         *is_active = true;
 
         let active = Arc::clone(&self.active);
 
         let tx: SyncSender<MidiMessage> = tx.clone();
+        let midi_in = MidiInput::new("MIDI input").unwrap();
+        let port = Midir::get_port(&midi_in, &port_pattern)?;
 
         let handle = thread::spawn(move || {
-
-            let midi_in = MidiInput::new("MIDI input").unwrap();
-            let ports = midi_in.ports();
-
-            // TODO: dynamically pick the port
-            let port = ports.get(3).unwrap();
-            let port_name = midi_in.port_name(port)
+            let port_name = midi_in
+                .port_name(&port)
                 .unwrap_or(String::from("(unknown port)"));
 
             // Assignment here is necessary to keep the connection alive.
             let _connection = midi_in.connect(
-                port,
+                &port,
                 port_name.as_str(),
-                move | _, bytes, _| {
+                move |_, bytes, _| {
                     if let Some(msg) = parse_message(bytes) {
                         let _ = tx.send(msg);
                     }
                 },
-                ()
+                (),
             );
 
             // Keep the thread alive until stop() is called
@@ -83,6 +100,13 @@ impl Midir {
         let mut is_active = active.lock().unwrap();
         *is_active = false;
     }
+
+    fn get_port(midi_in: &MidiInput, pattern: &str) -> Option<MidiInputPort> {
+        midi_in
+            .ports()
+            .iter()
+            .find(|p| midi_in.port_name(p).unwrap_or(String::from("")).contains(pattern)).cloned()
+    }
 }
 
 fn parse_message(bytes: &[u8]) -> Option<MidiMessage> {
@@ -91,49 +115,47 @@ fn parse_message(bytes: &[u8]) -> Option<MidiMessage> {
     let chan: u8 = status & 0x0F;
 
     return match FromPrimitive::from_u8((status & 0xF0) >> 4) {
-        Some(ChannelMessageType::NoteOff) => {
-            Some(MidiMessage::NoteOff {
-                channel: chan,
-                key: *bytes.get(1)?,
-                velocity: *bytes.get(2)?
-            })
-        }
+        Some(ChannelMessageType::NoteOff) => Some(MidiMessage::NoteOff {
+            channel: chan,
+            key: *bytes.get(1)?,
+            velocity: *bytes.get(2)?,
+        }),
 
         Some(ChannelMessageType::NoteOn) => Some(MidiMessage::NoteOn {
             channel: chan,
             key: *bytes.get(1)?,
-            velocity: *bytes.get(2)?
+            velocity: *bytes.get(2)?,
         }),
-
 
         Some(ChannelMessageType::PolyAftertouch) => Some(MidiMessage::PolyAftertouch {
             channel: chan,
             key: *bytes.get(1)?,
-            value: *bytes.get(2)?
+            value: *bytes.get(2)?,
         }),
 
         Some(ChannelMessageType::ControlChange) => Some(MidiMessage::ControlChange {
             channel: chan,
             control: *bytes.get(1)?,
-            value: *bytes.get(2)?
+            value: *bytes.get(2)?,
         }),
 
         Some(ChannelMessageType::ProgramChange) => Some(MidiMessage::ProgramChange {
             channel: chan,
-            program: *bytes.get(1)?
+            program: *bytes.get(1)?,
         }),
 
         Some(ChannelMessageType::ChannelAfterTouch) => Some(MidiMessage::ChannelAftertouch {
             channel: chan,
-            value: *bytes.get(1)?
+            value: *bytes.get(1)?,
         }),
 
         Some(ChannelMessageType::PitchBendChange) => Some(MidiMessage::PitchBendChange {
             channel: chan,
-            value: ((*bytes.get(1)? & 0b01111111u8) as u16) | (((*bytes.get(2)? as u16 & 0b01111111u16) << 7) as u16)
+            value: ((*bytes.get(1)? & 0b01111111u8) as u16)
+                | (((*bytes.get(2)? as u16 & 0b01111111u16) << 7) as u16),
         }),
 
         Some(ChannelMessageType::System) => Some(MidiMessage::Other),
         None => None,
-    }
+    };
 }
