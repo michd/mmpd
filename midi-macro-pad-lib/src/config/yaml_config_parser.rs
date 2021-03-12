3f1fe5d82e9c5e517a3a597950324bbc27f62c8f
Michael Duerinckx <mich@michd.me>
2021-03-12 14:53:13 +0000

    Document Yaml config parser, improve code style
    Now using functional style to parse Yaml values into RawConfig, looks
    tidier.

--- midi-macro-pad-lib/src/config/yaml_config_parser.rs
+++ midi-macro-pad-lib/src/config/yaml_config_parser.rs
@@ -1,80 +1,70 @@
+//! Parses YAML string into YAML representation tree, into `RawConfig` tree
+
 use std::str::FromStr;
-
-use linked_hash_map::LinkedHashMap;
-
 use crate::config::{ConfigError, ConfigInput, Loc};
 use crate::config::raw_config::RawConfig;
+extern crate yaml_rust;
+use yaml_rust::{Yaml, YamlLoader};
 
-use super::yaml_rust::{Yaml, YamlLoader};
-
+/// Config input parser, parsing YAML into `RawConfig`.
 pub struct YamlConfigParser<'a> {
+    /// Full config file contents
     input: &'a str
 }
 
 impl YamlConfigParser<'_> {
+    /// Creates a new parser instance from the full text contents of the configuration file
     pub fn new(input: &str) -> YamlConfigParser {
         YamlConfigParser { input }
     }
 }
 
 impl ConfigInput for YamlConfigParser<'_> {
+    /// Attempts parsing the YAML file contents into the intermediary `RawConfig` format.
+    /// If anything about it fails, returns a `ConfigError`.
     fn as_raw_config(&self) -> Result<RawConfig, ConfigError> {
-        let yaml_result = YamlLoader::load_from_str(self.input);
+        let mut yaml = YamlLoader::load_from_str(self.input).map_err(|err| {
+           ConfigError::FormatError(
+               err.to_string(),
+               Loc { line: err.marker().line(), col: err.marker().col() }
+           )
+        })?;
 
-        if let Err(err) = yaml_result {
-            return Err(ConfigError::FormatError(
-                err.to_string(),
-                Loc { line: err.marker().line(), col: err.marker().col() }
-            ));
-        }
-
-        let mut yaml = yaml_result.unwrap();
-
-        if yaml.is_empty() {
-            return Ok(RawConfig::Null)
-        }
-
-        let yaml = yaml.swap_remove(0);
-
-        Ok(yaml_to_rawconfig(yaml))
+        Ok(if yaml.is_empty() {
+            RawConfig::Null
+        } else {
+            yaml_to_raw_config(&yaml.swap_remove(0))
+        })
     }
 }
 
-fn yaml_to_rawconfig(yaml: Yaml) -> RawConfig {
+fn yaml_to_raw_config(yaml: &Yaml) -> RawConfig {
     match yaml {
-        Yaml::Real(real) => {
-            let fl = f64::from_str(&real);
+        Yaml::Real(real) => f64::from_str(real)
+            .map_or(
+                RawConfig::Null,
+                |f| RawConfig::Integer(f as i64)
+            ),
 
-            if let Ok(fl) = fl {
-                RawConfig::Integer(fl as i64)
-            } else {
-                RawConfig::Null
-            }
-        }
+        Yaml::Integer(i) => RawConfig::Integer(*i),
+        Yaml::String(s) => RawConfig::String(s.to_string()),
+        Yaml::Boolean(b) => RawConfig::Bool(*b),
 
-        Yaml::Integer(i) => RawConfig::Integer(i),
-        Yaml::String(s) => RawConfig::String(s),
-        Yaml::Boolean(b) => RawConfig::Bool(b),
+        Yaml::Array(arr) => RawConfig::Array(
+            arr
+                .iter()
+                .map( yaml_to_raw_config)
+                .collect()
+        ),
 
-        Yaml::Array(arr) => {
-            let mut rc_arr: Vec<RawConfig> = vec![];
-
-            for item in arr {
-                rc_arr.push(yaml_to_rawconfig(item));
-            }
-
-            RawConfig::Array(rc_arr)
-        }
-
-        Yaml::Hash(hash) => {
-            let mut rc_hash: LinkedHashMap<RawConfig, RawConfig> = LinkedHashMap::new();
-
-            for (key, value) in hash {
-                rc_hash.insert(yaml_to_rawconfig(key), yaml_to_rawconfig(value));
-            }
-
-            RawConfig::Hash(rc_hash)
-        }
+        Yaml::Hash(hash) => RawConfig::Hash(
+            hash
+                .iter()
+                .map(|(key, value)| {
+                    (yaml_to_raw_config(key), yaml_to_raw_config(value))
+                })
+                .collect()
+        ),
 
         Yaml::Alias(_) => RawConfig::Null,
         Yaml::Null => RawConfig::Null,
