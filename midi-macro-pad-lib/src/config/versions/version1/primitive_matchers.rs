a2a809cbd12afa99b56ec6fbe101f5adc747b625
Michael Duerinckx <mich@michd.me>
2021-03-13 13:01:12 +0000

    Add unit tests for config version1 primitive matchers

--- midi-macro-pad-lib/src/config/versions/version1/primitive_matchers.rs
+++ midi-macro-pad-lib/src/config/versions/version1/primitive_matchers.rs
@@ -128,7 +128,9 @@ pub fn build_number_matcher(matcher: Option<&RawConfig>) -> Result<Option<Number
                 if *i >= 0 {
                     Some(NumberMatcher::Val(*i as u32))
                 } else {
-                    None
+                    return Err(ConfigError::InvalidConfig(
+                       format!("Number matcher only supports positive integers, {} given", *i)
+                    ));
                 }
             },
 
@@ -204,5 +206,247 @@ pub fn build_number_matcher(matcher: Option<&RawConfig>) -> Result<Option<Number
         })
     } else {
         Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod string_matcher_tests {
+    use crate::config::raw_config::{RCHash, k};
+    use crate::config::versions::version1::primitive_matchers::build_string_matcher;
+    use crate::match_checker::StringMatcher;
+    use regex::Regex;
+
+    #[test]
+    fn builds_is_string_matcher() {
+        let mut input = RCHash::new();
+        input.insert(k("is"), k("match"));
+
+        let matcher = build_string_matcher(Some(&input))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher, StringMatcher::Is("match".to_string()))
+    }
+
+    #[test]
+    fn builds_contains_string_matcher() {
+        let mut input = RCHash::new();
+        input.insert(k("contains"), k("match"));
+
+        let matcher = build_string_matcher(Some(&input))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher, StringMatcher::Contains("match".to_string()));
+    }
+
+    #[test]
+    fn builds_starts_with_string_matcher() {
+        let mut input = RCHash::new();
+        input.insert(k("starts_with"), k("match"));
+
+        let matcher = build_string_matcher(Some(&input))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher, StringMatcher::StartsWith("match".to_string()));
+    }
+
+    #[test]
+    fn builds_ends_with_string_matcher() {
+        let mut input = RCHash::new();
+        input.insert(k("ends_with"), k("match"));
+
+        let matcher = build_string_matcher(Some(&input))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher, StringMatcher::EndsWith("match".to_string()));
+    }
+
+    #[test]
+    fn builds_regex_string_matcher() {
+        let mut input = RCHash::new();
+        input.insert(k("regex"), k("foo|bar"));
+
+        let matcher = build_string_matcher(Some(&input))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher, StringMatcher::Regex(Regex::new("foo|bar").ok().unwrap()));
+    }
+
+    #[test]
+    fn regex_string_matcher_returns_error_on_invalid_regex() {
+        let mut input = RCHash::new();
+        input.insert(k("regex"), k("foo(bar"));
+
+        let matcher = build_string_matcher(Some(&input));
+
+        assert!(matcher.is_err());
+    }
+
+    #[test]
+    fn builds_none_string_matcher_if_key_not_found() {
+        let mut input = RCHash::new();
+        input.insert(k("gobbledygook"), k("hello"));
+
+        let matcher = build_string_matcher(Some(&input));
+
+        assert!(if let Ok(None) = matcher { true } else { false });
+    }
+
+    #[test]
+    fn ignores_capitalisation_in_string_matcher_keys() {
+        let mut input_is = RCHash::new();
+        input_is.insert(k("Is"), k("match"));
+        let matcher_is = build_string_matcher(Some(&input_is))
+            .ok().unwrap().unwrap();
+
+        let mut input_contains = RCHash::new();
+        input_contains.insert(k("conTaInS"), k("match"));
+        let matcher_contains = build_string_matcher(Some(&input_contains))
+            .ok().unwrap().unwrap();
+
+        let mut input_starts_with = RCHash::new();
+        input_starts_with.insert(k("StArTs_WITH"), k("match"));
+        let matcher_starts_with = build_string_matcher(Some(&input_starts_with))
+            .ok().unwrap().unwrap();
+
+        let mut input_ends_with = RCHash::new();
+        input_ends_with.insert(k("Ends_WITH"), k("match"));
+        let matcher_ends_with = build_string_matcher(Some(&input_ends_with))
+            .ok().unwrap().unwrap();
+
+        let mut input_regex = RCHash::new();
+        input_regex.insert(k("REGEx"), k("match|other_match"));
+        let matcher_regex = build_string_matcher(Some(&input_regex))
+            .ok().unwrap().unwrap();
+
+
+        assert_eq!(matcher_is, StringMatcher::Is("match".to_string()));
+        assert_eq!(matcher_contains, StringMatcher::Contains("match".to_string()));
+        assert_eq!(matcher_starts_with, StringMatcher::StartsWith("match".to_string()));
+        assert_eq!(matcher_ends_with, StringMatcher::EndsWith("match".to_string()));
+        assert_eq!(matcher_regex, StringMatcher::Regex(Regex::new("match|other_match").unwrap()));
+    }
+}
+
+#[cfg(test)]
+mod number_matcher_tests {
+    use crate::config::raw_config::{RawConfig, RCHash, k};
+    use crate::config::versions::version1::primitive_matchers::build_number_matcher;
+    use crate::match_checker::NumberMatcher;
+
+    #[test]
+    fn build_val_number_matcher() {
+        let input_pos = RawConfig::Integer(7);
+        let matcher_pos = build_number_matcher(Some(&input_pos))
+            .ok().unwrap().unwrap();
+
+        let input_zero = RawConfig::Integer(0);
+        let matcher_zero = build_number_matcher(Some(&input_zero))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher_pos, NumberMatcher::Val(7));
+        assert_eq!(matcher_zero, NumberMatcher::Val(0));
+    }
+
+    #[test]
+    fn returns_an_error_on_negative_val() {
+        let input = RawConfig::Integer(-7);
+
+        let matcher = build_number_matcher(Some(&input));
+
+        assert!(matcher.is_err());
+    }
+
+    #[test]
+    fn builds_range_number_matcher() {
+        let mut input = RCHash::new();
+        input.insert(k("min"), RawConfig::Integer(7));
+        input.insert(k("max"), RawConfig::Integer(10));
+
+        let matcher = build_number_matcher(Some(&RawConfig::Hash(input)))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher, NumberMatcher::Range { min: Some(7), max: Some(10) });
+    }
+
+    #[test]
+    fn builds_open_ended_range_number_matcher() {
+        let mut input_min = RCHash::new();
+        input_min.insert(k("min"), RawConfig::Integer(7));
+        let matcher_min = build_number_matcher(Some(&RawConfig::Hash(input_min)))
+            .ok().unwrap().unwrap();
+
+        let mut input_max = RCHash::new();
+        input_max.insert(k("max"), RawConfig::Integer(10));
+        let matcher_max = build_number_matcher(Some(&RawConfig::Hash(input_max)))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(matcher_min, NumberMatcher::Range { min: Some(7), max: None });
+        assert_eq!(matcher_max, NumberMatcher::Range { min: None, max: Some(10) });
+    }
+
+    #[test]
+    fn returns_error_when_range_min_or_max_are_negative() {
+        let mut input_min_neg = RCHash::new();
+        input_min_neg.insert(k("min"), RawConfig::Integer(-3));
+        input_min_neg.insert(k("max"), RawConfig::Integer(9));
+        let matcher_min_neg = build_number_matcher(Some(&RawConfig::Hash(input_min_neg)));
+
+        let mut input_max_neg = RCHash::new();
+        input_max_neg.insert(k("min"), RawConfig::Integer(2));
+        input_max_neg.insert(k("max"), RawConfig::Integer(-9));
+        let matcher_max_neg = build_number_matcher(Some(&RawConfig::Hash(input_max_neg)));
+
+        let mut input_both_neg = RCHash::new();
+        input_both_neg.insert(k("min"), RawConfig::Integer(-3));
+        input_both_neg.insert(k("max"), RawConfig::Integer(-1));
+        let matcher_both_neg = build_number_matcher(Some(&RawConfig::Hash(input_both_neg)));
+
+        assert!(matcher_min_neg.is_err());
+        assert!(matcher_max_neg.is_err());
+        assert!(matcher_both_neg.is_err());
+    }
+
+    #[test]
+    fn returns_error_when_range_min_is_greater_than_max() {
+        let mut input = RCHash::new();
+        input.insert(k("min"), RawConfig::Integer(10));
+        input.insert(k("max"), RawConfig::Integer(7));
+
+        let matcher = build_number_matcher(Some(&RawConfig::Hash(input)));
+
+        assert!(matcher.is_err());
+    }
+
+    #[test]
+    fn builds_list_of_number_matchers() {
+        let mut range_input_hash = RCHash::new();
+        range_input_hash.insert(k("min"), RawConfig::Integer(12));
+        range_input_hash.insert(k("max"), RawConfig::Integer(18));
+
+        let input = RawConfig::Array(vec![
+            RawConfig::Integer(3),
+            RawConfig::Integer(7),
+            RawConfig::Hash(range_input_hash),
+            RawConfig::Array(vec![
+                RawConfig::Integer(23),
+                RawConfig::Integer(42)
+            ])
+        ]);
+
+        let matcher = build_number_matcher(Some(&input))
+            .ok().unwrap().unwrap();
+
+        assert_eq!(
+            matcher,
+            NumberMatcher::List(vec![
+                NumberMatcher::Val(3),
+                NumberMatcher::Val(7),
+                NumberMatcher::Range { min: Some(12), max: Some(18) },
+                NumberMatcher::List(vec![
+                    NumberMatcher::Val(23),
+                    NumberMatcher::Val(42)
+                ])
+            ])
+        )
     }
 }
